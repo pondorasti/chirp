34da7d178db3a327f25c4d1820606e5739a3a7ba
Alexandru Turcanu <pondorasti@gmail.com>
2022-10-30 23:29:11 -0400

    feat: twitter env var

--- www/lib/twitter.ts
+++ www/lib/twitter.ts
@@ -1,5 +1,10 @@
 import { Client } from "twitter-api-sdk"
 
-const client = new Client()
+const token = process.env.TWITTER_BEARER_TOKEN
+if (!token) {
+  throw new Error("TWITTER_BEARER_TOKEN is not defined")
+}
+
+const client = new Client(token)
 
 export default client
