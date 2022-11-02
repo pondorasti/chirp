bda5c92630db1e39a29091e02d75fcd10f9198c8
Alexandru Turcanu <pondorasti@gmail.com>
2022-11-01 18:36:01 -0700

    feat: icons group

---
+++ widget/widget-src/Icons.tsx
@@ -0,0 +1,21 @@
+export const refreshIcon = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+<polyline points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline>
+<path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
+</svg>
+`
+
+export const externalLinkIcon = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+<polyline points="15 3 21 3 21 9"></polyline>
+<line x1="10" y1="14" x2="21" y2="3"></line>
+</svg>`
+
+export const slidersIcon = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+<line x1="4" y1="21" x2="4" y2="14"></line>
+<line x1="4" y1="10" x2="4" y2="3"></line>
+<line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line>
+<line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line>
+<line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line>
+<line x1="17" y1="16" x2="23" y2="16"></line>
+</svg>
+`
--- widget/widget-src/code.tsx
+++ widget/widget-src/code.tsx
@@ -1,30 +1,10 @@
+import { externalLinkIcon, refreshIcon, slidersIcon } from "./Icons"
+
 const { widget } = figma
 const { useEffect, usePropertyMenu, useSyncedState, AutoLayout, Input, Text } = widget
 
-const refreshIcon = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-<polyline points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline>
-<path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
-</svg>
-`
-
-const externalLinkIcon = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-<polyline points="15 3 21 3 21 9"></polyline>
-<line x1="10" y1="14" x2="21" y2="3"></line>
-</svg>`
-
-const slidersIcon = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
-<line x1="4" y1="21" x2="4" y2="14"></line>
-<line x1="4" y1="10" x2="4" y2="3"></line>
-<line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line>
-<line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line>
-<line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line>
-<line x1="17" y1="16" x2="23" y2="16"></line>
-</svg>
-`
-
 function Widget() {
-  const [text, setText] = useSyncedState("text", "")
+  const [tweetIdInput, setTweetIdInput] = useSyncedState("tweetIdInput", "")
 
   usePropertyMenu(
     [
@@ -95,16 +75,15 @@ function Widget() {
           Tweet ID
         </Text>
         <Input
-          value={text}
-          placeholder="158539610883"
+          value={tweetIdInput}
+          placeholder="1585396100026208257"
           onTextEditEnd={(e) => {
-            setText(e.characters)
+            setTweetIdInput(e.characters)
           }}
           fontSize={16}
           fill="#525252"
           inputFrameProps={{
             fill: "#f5f5f5",
-            // stroke: "#e5e5e5",
             cornerRadius: 12,
             padding: 8,
           }}
@@ -129,8 +108,8 @@ function Widget() {
               console.log("boop")
               figma.showUI(__html__, { visible: false })
               figma.ui.postMessage({ type: "networkRequest" })
-              figma.ui.onmessage = (msg) => {
-                console.log({ msg })
+              figma.ui.onmessage = (tweet) => {
+                console.log({ tweet })
                 resolve(null)
               }
             })
