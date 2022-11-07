a86f1ffaa650dc868fe4bb75b633614cd7aed6b1
Alexandru Turcanu <pondorasti@gmail.com>
2022-11-07 12:13:20 -0800

    feat(www): background pattern

---
+++ www/app/FigmaIcon.tsx
@@ -0,0 +1,31 @@
+export default function FigmaIcon() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M16 4C16 5.65685 14.6569 7 13 7L10 7V1L13 1C14.6569 1 16 2.34315 16 4V4Z"
+        stroke="currentColor"
+        strokeWidth="1.75"
+      />
+      <path
+        d="M4 4C4 5.65685 5.34315 7 7 7L10 7V1L7 1C5.34315 1 4 2.34315 4 4V4Z"
+        stroke="currentColor"
+        strokeWidth="1.75"
+      />
+      <path
+        d="M4 10C4 11.6569 5.34315 13 7 13H10V7L7 7C5.34315 7 4 8.34315 4 10V10Z"
+        stroke="currentColor"
+        strokeWidth="1.75"
+      />
+      <path
+        d="M4 16C4 17.6569 5.34315 19 7 19V19C8.65685 19 10 17.6569 10 16V13H7C5.34315 13 4 14.3431 4 16V16Z"
+        stroke="currentColor"
+        strokeWidth="1.75"
+      />
+      <path
+        d="M10 10C10 11.6569 11.3431 13 13 13V13C14.6569 13 16 11.6569 16 10V10C16 8.34315 14.6569 7 13 7V7C11.3431 7 10 8.34315 10 10V10Z"
+        stroke="currentColor"
+        strokeWidth="1.75"
+      />
+    </svg>
+  )
+}
