c2d9d3fa83b610b12ecb6d5c5ce08790e3131d8b
Alexandru Turcanu <pondorasti@gmail.com>
2023-01-29 13:55:54 -0800

    feat: enable gyro

--- www/app/components/tilt.tsx
+++ www/app/components/tilt.tsx
@@ -1,4 +1,5 @@
 "use client"
+import clsx from "clsx"
 import React from "react"
 import Tilt from "react-parallax-tilt"
 
@@ -8,15 +9,18 @@ interface TiltWrapperProps extends React.PropsWithChildren {
 
 export const TiltWrapper: React.FC<Readonly<TiltWrapperProps>> = ({ className, children }) => (
   <Tilt
-    className={className}
+    className={clsx(className)}
     perspective={880}
-    scale={1.05}
     trackOnWindow={false}
+    tiltMaxAngleX={5}
+    tiltMaxAngleY={5}
     transitionSpeed={600}
-    // glareEnable
-    // glarePosition="all"
-    // glareMaxOpacity={0.9}
-    // glareColor="white"
+    gyroscope
+    glareEnable
+    glarePosition="all"
+    glareMaxOpacity={0.08}
+    glareBorderRadius="32px"
+    glareColor="#1DA1F2"
   >
     {children}
   </Tilt>
--- www/app/page.tsx
+++ www/app/page.tsx
@@ -52,30 +52,29 @@ const IntentGroup = ({
   href: string
   foregroundColor: string
   backgroundColor: string
-}) => {
-  return (
-    <a
-      className={clsx(
-        "flex flex-row gap-1 items-center cursor-pointer group/intent text-twitter-gray fill-twitter-gray",
-        foregroundColor
-      )}
-      href={href}
-      target="_blank"
-      rel="noreferrer"
-    >
-      <div aria-hidden className={clsx("p-[7px] rounded-full", backgroundColor)}>
-        {icon}
-      </div>
-      <span className="leading-none text-[13px]">
-        {count > 1000 ? `${(count / 1000).toFixed(1)}K` : count.toLocaleString()}
-      </span>
-      <span className="sr-only">{label}</span>
-    </a>
-  )
-}
+}) => (
+  <a
+    className={clsx(
+      "flex flex-row gap-1 items-center cursor-pointer group/intent text-twitter-gray fill-twitter-gray",
+      foregroundColor
+    )}
+    href={href}
+    target="_blank"
+    rel="noreferrer"
+  >
+    <div aria-hidden className={clsx("p-[7px] rounded-full", backgroundColor)}>
+      {icon}
+    </div>
+    <span className="leading-none text-[13px]">
+      {count > 1000 ? `${(count / 1000).toFixed(1)}K` : count.toLocaleString()}
+    </span>
+    <span className="sr-only">{label}</span>
+  </a>
+)
 
-const PARALLAX_STYLE =
-  "group-hover:[transform:translateZ(30px)] transition-transform duration-[600ms]"
+const TRANSFORM_ANIMATION =
+  "transition-transform duration-[600ms] [transition-timing-function:ease] will-change-transform"
+const PARALLAX_STYLE = "group-hover:[transform:translateZ(30px)]"
 
 export default async function Home() {
   const tweets = await getTweets()
@@ -89,94 +88,105 @@ export default async function Home() {
           Embed any tweets in figma
         </p>
 
-        <TiltWrapper className="[transform-style:preserve-3d] group bg-white p-8 flex flex-col gap-5 rounded-[32px] shadow-lg font-inter max-w-md">
-          <div className={clsx("flex gap-3 items-center", PARALLAX_STYLE)}>
-            {tweet.author.profileImageURI && (
+        <div
+          className={clsx(
+            "hover:scale-[1.02] transition-transform duration-[600ms] [transition-timing-function:ease]",
+            TRANSFORM_ANIMATION
+          )}
+        >
+          <TiltWrapper className="[transform-style:preserve-3d] group bg-white p-8 flex flex-col gap-5 rounded-[32px] shadow-lg hover:shadow-xl font-inter max-w-md">
+            <div className={clsx("flex gap-3 items-center", PARALLAX_STYLE, TRANSFORM_ANIMATION)}>
+              {tweet.author.profileImageURI && (
+                <a
+                  href={`https://twitter.com/${tweet.author.username}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src={tweet.author.profileImageURI}
+                    alt={`${tweet.author.name}'s profile picture`}
+                    width={48}
+                    height={48}
+                    className="rounded-full"
+                    priority
+                  />
+                </a>
+              )}
+              <div className="flex flex-col">
+                <a
+                  href={`https://twitter.com/${tweet.author.username}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <h3 className="text-md font-medium leading-5 underline-offset-2 hover:underline">
+                    {tweet.author.name}
+                  </h3>
+                </a>
+                <a
+                  href={`https://twitter.com/${tweet.author.username}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <h4 className="text-twitter-gray pt-0.5 leading-5">@{tweet.author.username}</h4>
+                </a>
+              </div>
+
               <a
-                href={`https://twitter.com/${tweet.author.username}`}
+                className="text-twitter-brand-blue ml-auto"
+                href={`https://twitter.com/${tweet?.author.username}/status/${tweet?.id}`}
                 target="_blank"
                 rel="noreferrer"
               >
-                <Image
-                  src={tweet.author.profileImageURI}
-                  alt={`${tweet.author.name}'s profile picture`}
-                  width={48}
-                  height={48}
-                  className="rounded-full"
-                  priority
-                />
-              </a>
-            )}
-            <div className="flex flex-col">
-              <a
-                href={`https://twitter.com/${tweet.author.username}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <h3 className="text-md font-medium leading-5 underline-offset-2 hover:underline">
-                  {tweet.author.name}
-                </h3>
-              </a>
-              <a
-                href={`https://twitter.com/${tweet.author.username}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <h4 className="text-twitter-gray pt-0.5 leading-5">@{tweet.author.username}</h4>
+                <TwitterIcon />
               </a>
             </div>
 
-            <a
-              className="text-twitter-brand-blue ml-auto"
-              href={`https://twitter.com/${tweet?.author.username}/status/${tweet?.id}`}
-              target="_blank"
-              rel="noreferrer"
+            <p className={clsx("text-lg", PARALLAX_STYLE, TRANSFORM_ANIMATION)}>{tweet.text}</p>
+
+            <div
+              className={clsx(
+                "flex justify-between items-center w-full -m-[7px]",
+                PARALLAX_STYLE,
+                TRANSFORM_ANIMATION
+              )}
             >
-              <TwitterIcon />
-            </a>
-          </div>
-
-          <p className={clsx("text-lg", PARALLAX_STYLE)}>{tweet.text}</p>
-
-          <div
-            className={clsx("flex justify-between items-center w-full -m-[7px]", PARALLAX_STYLE)}
-          >
-            <IntentGroup
-              icon={<ReplyIcon />}
-              count={tweet.publicMetrics.replyCount}
-              label="replies"
-              foregroundColor="hover:text-twitter-blue"
-              backgroundColor="group-hover/intent:bg-twitter-faded-blue"
-              href={`https://twitter.com/intent/tweet?in_reply_to=${tweet.id}`}
-            />
-            <IntentGroup
-              icon={<HeartIcon />}
-              count={tweet.publicMetrics.likeCount}
-              label="likes"
-              foregroundColor="hover:text-twitter-red"
-              backgroundColor="group-hover/intent:bg-twitter-faded-red"
-              href={`https://twitter.com/intent/like?tweet_id=${tweet.id}`}
-            />
-            <IntentGroup
-              icon={<RetweetIcon />}
-              count={tweet.publicMetrics.retweetCount}
-              label="retweets"
-              foregroundColor="hover:text-twitter-green"
-              backgroundColor="group-hover/intent:bg-twitter-faded-green"
-              href={`https://twitter.com/intent/retweet?tweet_id=${tweet.id}`}
-            />
-            {tweet.publicMetrics.impressionCount > 0 && (
               <IntentGroup
-                icon={<ImpressionIcon />}
-                count={tweet.publicMetrics.impressionCount}
-                label="impressions"
+                icon={<ReplyIcon />}
+                count={tweet.publicMetrics.replyCount}
+                label="replies"
                 foregroundColor="hover:text-twitter-blue"
                 backgroundColor="group-hover/intent:bg-twitter-faded-blue"
-                href={`https://twitter.com/${tweet?.author.username}/status/${tweet?.id}`}
+                href={`https://twitter.com/intent/tweet?in_reply_to=${tweet.id}`}
               />
-            )}
-          </div>
-        </TiltWrapper>
+              <IntentGroup
+                icon={<HeartIcon />}
+                count={tweet.publicMetrics.likeCount}
+                label="likes"
+                foregroundColor="hover:text-twitter-red"
+                backgroundColor="group-hover/intent:bg-twitter-faded-red"
+                href={`https://twitter.com/intent/like?tweet_id=${tweet.id}`}
+              />
+              <IntentGroup
+                icon={<RetweetIcon />}
+                count={tweet.publicMetrics.retweetCount}
+                label="retweets"
+                foregroundColor="hover:text-twitter-green"
+                backgroundColor="group-hover/intent:bg-twitter-faded-green"
+                href={`https://twitter.com/intent/retweet?tweet_id=${tweet.id}`}
+              />
+              {tweet.publicMetrics.impressionCount > 0 && (
+                <IntentGroup
+                  icon={<ImpressionIcon />}
+                  count={tweet.publicMetrics.impressionCount}
+                  label="impressions"
+                  foregroundColor="hover:text-twitter-blue"
+                  backgroundColor="group-hover/intent:bg-twitter-faded-blue"
+                  href={`https://twitter.com/${tweet?.author.username}/status/${tweet?.id}`}
+                />
+              )}
+            </div>
+          </TiltWrapper>
+        </div>
 
         <a
           className="flex items-center justify-center px-6 py-3 bg-twitter-blue rounded-full text-gray-50 gap-2 font-semibold text-lg w-fit mx-auto cursor-pointer mt-16 mb-20"
